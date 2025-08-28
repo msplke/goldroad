2890b7c056c95ccaab1233bc499f7b1d78077ace
Mirror83 <glenochieng045@gmail.com>
2025-08-28 14:03:29 +0300

    chore: add ngrok url to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const config: NextConfig = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  allowedDevOrigins: ["*.ngrok-free.app"],
 };
 
 export default config;
