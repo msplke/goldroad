4d81e2d63838ed7f4a470532974cced790d7ab80
Mirror83 <glenochieng045@gmail.com>
2025-08-28 13:49:39 +0300

    fix: strengthen data integrity constraints on the paid_subscriber table

--- src/server/db/schema/app-schema.ts
+++ src/server/db/schema/app-schema.ts
@@ -7,11 +7,21 @@ import { createdAt, createTable, updatedAt } from "~/server/db/schema/utils";
 
 export const paid_subscriber = createTable("paid_subscribers", (d) => ({
   id: d.uuid().primaryKey().defaultRandom(),
-  email: d.text("email"),
-  firstName: d.text("first_name").default(""),
-  paystackSubscriptionCode: d.text("paystack_subscription_code"),
-  kitSubscriberId: d.bigint("kit_subscriber_id", { mode: "number" }),
-  status: d.varchar("status", { length: 20 }).$type<PaymentStatus>(),
+  email: d.text("email").unique().notNull(),
+  firstName: d.text("first_name"),
+  paystackSubscriptionCode: d
+    .text("paystack_subscription_code")
+    .unique()
+    .notNull(),
+  kitSubscriberId: d
+    .bigint("kit_subscriber_id", { mode: "number" })
+    .unique()
+    .notNull(),
+  status: d
+    .varchar("status", { length: 20 })
+    .$type<PaymentStatus>()
+    .default("paid")
+    .notNull(),
   createdAt,
   updatedAt,
 }));
