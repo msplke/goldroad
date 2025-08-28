8e06cf48694d508bf3f417701ad07d2c7826d029
Mirror83 <glenochieng045@gmail.com>
2025-08-28 23:07:40 +0300

    feat: add creator table

--- src/server/db/schema/app-schema.ts
+++ src/server/db/schema/app-schema.ts
@@ -2,7 +2,10 @@
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
 // db/schema.ts
+import { relations } from "drizzle-orm";
+
 import type { PaymentStatus } from "~/env";
+import { user } from "~/server/db/schema/auth-schema";
 import { createdAt, createTable, updatedAt } from "~/server/db/schema/utils";
 
 export const paidSubscriber = createTable("paid_subscriber", (d) => ({
@@ -24,4 +27,36 @@ export const paidSubscriber = createTable("paid_subscriber", (d) => ({
     .notNull(),
   createdAt,
   updatedAt,
+}));
+
+export const tagInfo = createTable("tag_info", (d) => ({
+  id: d.uuid().defaultRandom().primaryKey(),
+  creatorId: d
+    .text()
+    .notNull()
+    .references(() => creator.id),
+  kit_active_tag_id: d.bigint({ mode: "number" }).notNull(),
+  kit_non_renewing_tag_id: d.bigint({ mode: "number" }).notNull(),
+  kit_attention_tag_id: d.bigint({ mode: "number" }).notNull(),
+  kit_completed_tag_id: d.bigint({ mode: "number" }).notNull(),
+  kit_cancelled_tag_id: d.bigint({ mode: "number" }).notNull(),
+  createdAt,
+  updatedAt,
+}));
+
+export const creator = createTable("creator", (d) => ({
+  id: d.uuid().defaultRandom().primaryKey(),
+  userId: d
+    .text()
+    .notNull()
+    .references(() => user.id),
+  // To be encrypted
+  kitApiKey: d.text().notNull(),
+  paystackSubaccountId: d.bigint({ mode: "number" }).notNull(),
+  createdAt,
+  updatedAt,
+}));
+
+export const creatorRelations = relations(creator, ({ one }) => ({
+  tagInfo: one(tagInfo),
 }));
