ead9e7e43b4f595d782a1b3c99a458e081f89dfe
Mirror83 <glenochieng045@gmail.com>
2025-09-04 15:08:44 +0300

    fix: separate existence check from creation
    also rethrow error so that failures are not masked

--- src/server/trigger/tasks.ts
+++ src/server/trigger/tasks.ts
@@ -1,7 +1,10 @@
 import { logger, schemaTask } from "@trigger.dev/sdk";
 import z from "zod";
 
-import { createSubscriber } from "~/server/actions/webhooks/paystack";
+import {
+  createSubscriber,
+  getSubscriberInfoBySubscriptionCode,
+} from "~/server/actions/webhooks/paystack";
 import { db } from "~/server/db";
 import { kitSubscriberCreateSchema } from "~/server/fetch-clients/kit";
 
@@ -15,22 +18,31 @@ export const createSubscriberTask = schemaTask({
   // Set an optional maxDuration to prevent tasks from running indefinitely
   maxDuration: 420, // Stop executing after 420 secs (7 mins) of compute
   run: async (payload, { ctx }) => {
-    logger.log("Creating subscriber...", { payload, ctx });
+    logger.log("Creating subscriber...", { ctx });
 
     try {
+      // Check if subscriber exists on DB
+      const subscriberExists = await getSubscriberInfoBySubscriptionCode(
+        db,
+        payload.subscriptionCode
+      );
+
+      if (subscriberExists) {
+        return { message: "Subscriber already exists." };
+      }
+
       await createSubscriber(
         db,
         payload.subscriberInfo,
         payload.subscriptionCode,
         payload.planCode
       );
+
+      return { message: "Successfully created subscriber" };
     } catch (error) {
       console.error(error);
-      logger.log("Error occurred, unable to create subscriber");
+      logger.error("Error occurred, unable to create subscriber", { error });
+      throw error;
     }
-
-    return {
-      message: "Successfully created subscriber",
-    };
   },
 });
