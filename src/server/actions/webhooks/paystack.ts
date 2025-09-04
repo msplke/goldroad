a4c3502871bd90677a67a4fe124a844ab9707476
Mirror83 <glenochieng045@gmail.com>
2025-09-04 11:26:08 +0300

    fix: match `createSubscriber` to updated schema

--- src/server/actions/webhooks/paystack.ts
+++ src/server/actions/webhooks/paystack.ts
@@ -1,11 +1,18 @@
+import { eq } from "drizzle-orm";
 import "server-only";
 
 /** These are actions that are to be executed only in Paystack webhooks */
 
 import type z from "zod";
 
-import { db } from "~/server/db";
-import { paidSubscriber } from "~/server/db/schema/app-schema";
+import type { DbType } from "~/server/db";
+import {
+  creator,
+  paidSubscriber,
+  plan,
+  publication,
+  tagInfo,
+} from "~/server/db/schema/app-schema";
 import {
   kitClient,
   type kitSubscriberCreateSchema,
@@ -15,29 +22,192 @@ import {
  * be usable
  */
 export async function createSubscriber(
+  db: DbType,
   subscriberInfo: z.infer<typeof kitSubscriberCreateSchema>,
   subscriptionCode: string,
+  planCode: string,
 ) {
-  // Create subscriber on Kit. The API upserts the user if they exist.
-  const { data: createSubscriberRes } = await kitClient("@post/subscribers/", {
-    body: subscriberInfo,
-    throws: true,
-  });
+  // Check if subscriber exists on DB
+  const subscriberExists = await getSubscriberInfoBySubscriptionCode(
+    db,
+    subscriptionCode,
+  );
 
-  if (createSubscriberRes === null) throw new Error("No data");
-
-  const subscriber = createSubscriberRes.subscriber;
+  if (subscriberExists) {
+    return;
+  }
 
   // Get the id of the tag that shows that a payment has been done
-  // For now, it is done by just using an environment variable
+  const { intervalTag, statusTag, publicationTag, planId, kitApiKey } =
+    await getCreatorInfoFromPlanCode(db, planCode);
 
-  // Tag the created user appropriately
+  if (!kitApiKey) {
+    // This ideally should never be called, since at this point the creator should
+    // have filled out all the required details, and thus the Kit API key should be set.
+    throw new Error(
+      "Unable to create subscriber. Creator Kit API Key not set!",
+    );
+  }
+
+  // Create subscriber on Kit. The API upserts the user if they exist.
+  const { data: createSubscriberKitRes } = await kitClient(
+    "@post/subscribers/",
+    {
+      body: subscriberInfo,
+      throws: true,
+      headers: {
+        "X-Kit-Api-Key": kitApiKey,
+      },
+    },
+  );
+
+  if (createSubscriberKitRes === null)
+    throw new Error("Unable to create subscriber on Kit");
+
+  const kitSubscriber = createSubscriberKitRes.subscriber;
+
+  // Tag the created user appropriately (failures will have to be resolved manually)
+  // This could be simplified by using the bulk tag addition Kit API endpoint, but
+  // that requires implementing Kit OAuth, which has not been done yet.
+  const tagResults = await Promise.all([
+    await tagSubscriberOnKit(
+      intervalTag.toString(),
+      kitSubscriber.id.toString(),
+      kitApiKey,
+    ),
+    await tagSubscriberOnKit(
+      publicationTag.toString(),
+      kitSubscriber.id.toString(),
+      kitApiKey,
+    ),
+    await tagSubscriberOnKit(
+      statusTag.toString(),
+      kitSubscriber.id.toString(),
+      kitApiKey,
+    ),
+  ]);
+
+  console.log("Tag results:", tagResults);
+
+  // Add the subscriber to the database
+  await db
+    .insert(paidSubscriber)
+    .values({
+      planId,
+      email: subscriberInfo.email_address,
+      firstName: subscriberInfo.first_name ?? "",
+      paystackSubscriptionCode: subscriptionCode,
+      kitSubscriberId: kitSubscriber.id,
+      status: "active",
+    })
+    .onConflictDoNothing({
+      target: paidSubscriber.kitSubscriberId,
+    });
+}
+
+export async function getSubscriberInfoBySubscriptionCode(
+  db: DbType,
+  paystackSubscriptionCode: string,
+) {
+  // Check if subscriber exists in app db
+  const foundSubscriber = await db.query.paidSubscriber.findFirst({
+    where: eq(
+      paidSubscriber.paystackSubscriptionCode,
+      paystackSubscriptionCode,
+    ),
+  });
+
+  return foundSubscriber;
+}
+
+async function getCreatorInfoFromPlanCode(
+  db: DbType,
+  paystackPlanCode: string,
+) {
+  const foundPlan = await db.query.plan.findFirst({
+    where: eq(plan.paystackPlanCode, paystackPlanCode),
+    columns: {
+      id: true,
+      publicationId: true,
+      interval: true,
+    },
+  });
+
+  if (!foundPlan) {
+    throw new Error("Could not find corresponding plan on the DB");
+  }
+
+  const foundPublication = await db.query.publication.findFirst({
+    where: eq(publication.id, foundPlan.publicationId),
+    columns: {
+      creatorId: true,
+      kitPublicationTagId: true,
+    },
+  });
+
+  if (!foundPublication) {
+    throw new Error("Unable to find creator for this plan");
+  }
+
+  const creatorInfo = await db.query.creator.findFirst({
+    where: eq(creator.id, foundPublication.creatorId),
+    columns: {
+      kitApiKey: true,
+    },
+  });
+
+  if (!creatorInfo) {
+    throw new Error("Unable to find creator for publication");
+  }
+
+  const foundTagInfo = await db.query.tagInfo.findFirst({
+    where: eq(tagInfo.creatorId, foundPublication.creatorId),
+  });
+
+  if (!foundTagInfo) {
+    throw new Error("Unable to retrieve tag info");
+  }
+
+  let intervalTag: number;
+
+  switch (foundPlan.interval) {
+    case "annually":
+      intervalTag = foundTagInfo.kitAnnualSubscriberTag;
+      break;
+    case "monthly":
+      intervalTag = foundTagInfo.kitMonthlySubscriberTag;
+      break;
+    case "daily":
+      intervalTag = foundTagInfo.kitDailySubscriberTag;
+      break;
+    case "hourly":
+      intervalTag = foundTagInfo.kitHourlySubscriberTag;
+      break;
+  }
+
+  return {
+    intervalTag,
+    publicationTag: foundPublication.kitPublicationTagId,
+    statusTag: foundTagInfo.kitActiveTagId,
+    planId: foundPlan.id,
+    kitApiKey: creatorInfo.kitApiKey,
+  };
+}
+
+async function tagSubscriberOnKit(
+  tag: string,
+  subscriberId: string,
+  kitApiKey: string,
+) {
   const { data: tagSubscriberRes } = await kitClient(
     "@post/tags/:tagId/subscribers/:subscriberId",
     {
       params: {
-        tagId: "useCodeFromDbNotThisCode",
-        subscriberId: subscriber.id.toString(),
+        tagId: tag,
+        subscriberId: subscriberId,
+      },
+      headers: {
+        "X-Kit-Api-Key": kitApiKey,
       },
       throws: true,
     },
@@ -47,17 +217,5 @@ export async function createSubscriber(
     throw new Error("No data");
   }
 
-  // Add the subscriber to the database
-  await db
-    .insert(paidSubscriber)
-    .values({
-      email: subscriber.email_address,
-      firstName: subscriber.first_name ?? "",
-      paystackSubscriptionCode: subscriptionCode,
-      kitSubscriberId: subscriber.id,
-      status: "active",
-    })
-    .onConflictDoNothing({
-      target: paidSubscriber.kitSubscriberId,
-    });
+  return tagSubscriberRes;
 }
