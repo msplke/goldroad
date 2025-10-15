8f6f89abe122c33f19e9b348752532aadce770f9
Mirror83 <glenochieng045@gmail.com>
2025-10-15 08:54:01 +0300

    refactor: move constants to constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,9 @@
 
 /** Maximum number of benefits allowed per plan */
 export const MAX_BENEFITS_PER_PLAN = 4;
+
+export const PAYSTACK_BASE_URL = "https://api.paystack.co";
+
+/**The amount being taken by the business as a percentage of the total transaction
+ * when a payment is made to a subaccount*/
+export const SUBACCOUNT_PERCENTAGE_CHARGE = 5; // i.e. subaccount gets 95% of the transaction
