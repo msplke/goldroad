c19f67a9f08ab4f3865d686f459814a0867e20fa
Mirror83 <glenochieng045@gmail.com>
2025-10-21 11:14:14 +0300

    feat: add webhook event handler for one-time payment
    the required fields are checked using a Zod schema rather than if statements as in the other handlers; I'm considering doing this for the other handlers as well.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -92,3 +92,7 @@ export function generateSlugFromName(name: string): string {
 
   return slug;
 }
+
+export function objectIsEmpty(obj: Record<string, unknown>): boolean {
+  return Object.keys(obj).length === 0;
+}
